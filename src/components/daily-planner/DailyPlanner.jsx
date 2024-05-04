fa404073a8ec173488f3b0e386030800e7efa772
Diana Papp <pappdiana148@gmail.com>
2024-05-04 17:38:14 +0300

    style components

--- src/components/daily-planner/DailyPlanner.jsx
+++ src/components/daily-planner/DailyPlanner.jsx
@@ -1,13 +1,14 @@
 import './DailyPlanner.css';
 import ToDoList from '../to-do-list/ToDoList';
 
+
 export default function DailyPlanner() {
   
 
     return (
       <>
         <div className='daily-elem'>
-          <h2 className="journal-title">Daily planner</h2>
+          <h2 className="planner-title">Daily planner</h2>
 
           <div className='planner-container'>
             
@@ -17,34 +18,44 @@ export default function DailyPlanner() {
 
               <form className="form-planner" action="">
 
-                <div className="section-one">
-                  <fieldset className="fieldset">
-                    <label className="label-text" htmlFor="wins">Daily wins </label>
+                <div className="section">
+                  <div className="planner-card">
+                    <label className="label-planner" htmlFor="wins">Daily wins </label>
                     <textarea className=" t-planner" name="wins" id="wins" cols="30" rows="10"></textarea>
-                  </fieldset>
+                  </div>
 
-                  <fieldset className="fieldset">
-                    <label className="label-text" htmlFor="challenges">Daily challenges </label>
+                  <div className="planner-card">
+                    <label className="label-planner" htmlFor="challenges">Daily challenges </label>
                     <textarea className=" t-planner" name="challenges" id="challenges" cols="30" rows="10"></textarea>
-                  </fieldset>
+                  </div>
 
-                  <fieldset className="fieldset">
-                    <label className="label-text" htmlFor="adjustments">Daily adjustments </label>
+                  <div className="planner-card">
+                    <label className="label-planner" htmlFor="adjustments">Daily adjustments </label>
                     <textarea className=" t-planner" name="adjustments" id="adjustments" cols="30" rows="10"></textarea>
-                  </fieldset>
+                  </div>
                 </div>
 
-                <div className='section-two'>
-                    <fieldset className="fieldset">
-                      <label className="label-text" htmlFor="reflections">Reflections </label>
-                      <textarea className=" t-planner" name="reflections" id="reflections" cols="30" rows="10"></textarea>
-                    </fieldset>
-                </div>
 
-                <div className="buttons-container">
+                <div className="section">
+                  <div className="planner-card planner-img img1"></div>
+                  <div className="planner-card">
+                    <label className="label-planner" htmlFor="reflections">Reflections </label>
+                    <textarea className=" t-planner" name="reflections" id="reflections" cols="30" rows="10"></textarea>
+                  </div>
+                  <div className="planner-card planner-img img2"></div>
+                </div>
+                  
+                
+                  
+
+                <div className="section btn-cont ">
                     <button className="button-planner">Save</button>
                     <button className="button-planner">Update</button>
                 </div>
+                
+
+                
+                
               
               </form>
           </div> 
